09afb9500646c1dc87b2709f73371534ec3b2471
SHARKY-CODER <i.toufik.x@gmail.com>
2026-07-24 10:21:55 +0530

    Add files via upload

---
+++ T.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int x;
+	char a[20];
+	float z;
+	printf("ENTER YOUR AGE");
+	scanf("%d",&x);
+	printf("ENTER YOUR NAME");
+	scanf("%s",&a);
+	printf("ENTER YOUR CGPA");
+	scanf("%f",&z);
+	printf("NAME=%s\n",a);
+	printf("AGE=%d\n",x);
+    printf("CGPA=%2f",z);
+
+
+}
